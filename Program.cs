ad8608eeba9785376181625ddcddc854f831a6bb
Lucas Apalosqui <70145530+lucasApalosqui@users.noreply.github.com>
2025-02-21 14:45:41 -0300

    Exercism - Orm in One Go

--- Program.cs
+++ Program.cs
@@ -1,130 +1,40 @@
 ﻿using System;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Collections.Generic;
-using System.Linq;
 
-public class LogParser
+public class Orm
 {
-    public bool IsValidLine(string text)
+    private Database database;
+
+    public Orm(Database database)
     {
-        var logline = text.Substring(0, 5);
-        if (logline.Contains("[") && logline.Contains("]"))
-            return (Enum.TryParse<Log>(logline.Substring(1, 3).ToUpper(), out var result)) ? true : false;
-        return false;
+        this.database = database;
     }
 
-    public string[] SplitLogLine(string text)
+    public void Write(string data)
     {
-        var builder = new StringBuilder();
-        bool verify = false;
-        foreach (var cha in text)
+        using (database)
         {
-            if (cha == '<') verify = true;
-            builder.Append(cha switch
-            {
-                '>' => '&',
-                _ when verify == true => null,
-                _ => cha
-            });
-            if (cha == '>') verify = false;
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
         }
-        return builder.ToString().Split('&');
     }
 
-    public int CountQuotedPasswords(string lines)
+    public bool WriteSafely(string data)
     {
-        int count = 0;
-        var separateStr = SepareStringByQuotes(lines);
-        foreach (string strings in separateStr)
-            if(strings.ToUpper().Contains("PASSWORD")) count++;
-        return count;
-    }
-
-    public string RemoveEndOfLineText(string line)
-    {
-        var Words = line.Split(' ');
-        string cleanLine = "";
-        for (int i = 0; i < Words.Length; i++)
+        using (database)
         {
-            if (!Words[i].Contains("end-of-line")) cleanLine += Words[i] + " ";
-            else if (Words[i] != Words[Words.Length - 1]) cleanLine += " ";
-        }
-           
-        return cleanLine;    
-    }
-
-    public string[] ListLinesWithPasswords(string[] lines)
-    {
-        string[] strings = new string[lines.Length];
-        string refactLine = "";
-        string passW = "";
-        int count = 0;
-        foreach (string line in lines)
-        {
-            var words = line.Split(" ");
-            int lastCont = 1;
-            foreach (string word in words)
+            try
             {
-                
-                if (word.ToLower().Contains("password"))
-                {
-                    passW = (word.ToLower().Equals("password")) ? "--------: " : $"{word}: ";
-                }
-                refactLine += (lastCont != words.Length) ? $"{word} " : word;
-                lastCont++;
+                database.BeginTransaction();
+                database.Write(data);
+                database.EndTransaction();
+                return true;
             }
-            lastCont = 1;
-            strings[count] = passW + refactLine;
-            count++;
-            refactLine = "";
-        }
-        return strings;
-    }
-
-
-    // Utils
-    private List<string> SepareStringByQuotes(string lines)
-    {
-        List<string> separateStr = new List<string>();
-        var build = new StringBuilder();
-        int quoteCount = 0;
-
-        foreach (char line in lines)
-        {
-            build.Append(line switch
+            catch (InvalidOperationException ex)
             {
-                '"' => null,
-                _ when char.IsControl(line) => null,
-                _ => line
-            });
-
-            if (line == '"')
-            {
-                quoteCount++;
-                if (quoteCount == 1 && build.Length != 0)
-                    build = new StringBuilder();
-
-                if (quoteCount == 2)
-                {
-                    separateStr.Add(build.ToString());
-                    build = new StringBuilder();
-                    quoteCount = 0;
-                }
-
+                database.Dispose();
+                return false;
             }
-
         }
-        return separateStr;
     }
-}
-
-public enum Log
-{
-    TRC,
-    DBG,
-    INF,
-    WRN,
-    ERR,
-    FTL
 }
