e2d5b7c94fbe808946c0d73838b96f5bb3ed5525
Lucas Apalosqui <70145530+lucasApalosqui@users.noreply.github.com>
2025-02-24 15:04:08 -0300

    Exercism - Developer Privileges

--- Program.cs
+++ Program.cs
@@ -1,40 +1,45 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
-public class Orm
+public class Authenticator
 {
-    private Database database;
-
-    public Orm(Database database)
+    // TODO: Implement the Authenticator.Admin property
+    public Identity Admin = new Identity()
     {
-        this.database = database;
-    }
+        Email = "admin@ex.ism",
+        FacialFeatures = new FacialFeatures { EyeColor = "green", PhiltrumWidth = 0.9M },
+        NameAndAddress = new List<string> { "Chanakya", "Mumbai", "India" }
+    };
 
-    public void Write(string data)
+    // TODO: Implement the Authenticator.Developers property
+    public IDictionary<string, Identity> Developers = new Dictionary<string, Identity>
     {
-        using (database)
-        {
-            database.BeginTransaction();
-            database.Write(data);
-            database.EndTransaction();
-        }
-    }
-
-    public bool WriteSafely(string data)
-    {
-        using (database)
-        {
-            try
+        { "Bertrand", new Identity
             {
-                database.BeginTransaction();
-                database.Write(data);
-                database.EndTransaction();
-                return true;
-            }
-            catch (InvalidOperationException ex)
+                Email = "bert@ex.ism",
+                FacialFeatures = new FacialFeatures { EyeColor = "blue", PhiltrumWidth = 0.8M },
+                NameAndAddress = new List<string> { "Bertrand", "Paris", "France" }
+            }},
+        { "Anders", new Identity
             {
-                database.Dispose();
-                return false;
-            }
-        }
-    }
+                Email = "anders@ex.ism",
+                FacialFeatures = new FacialFeatures { EyeColor = "brown", PhiltrumWidth = 0.85M },
+                NameAndAddress = new List<string> { "Anders", "Redmond", "USA" }
+            }}
+    };
+}
+
+//**** please do not modify the FacialFeatures class ****
+public class FacialFeatures
+{
+    public required string EyeColor { get; set; }
+    public required decimal PhiltrumWidth { get; set; }
+}
+
+//**** please do not modify the Identity class ****
+public class Identity
+{
+    public required string Email { get; set; }
+    public required FacialFeatures FacialFeatures { get; set; }
+    public required IList<string> NameAndAddress { get; set; }
 }
